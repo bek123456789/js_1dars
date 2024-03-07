160e0a2891ff4a0de6e20dbf9fc4665f0b316acf
bek123456789 <redmirating@gmail.com>
2024-03-07 15:19:22 +0500

    done

---
+++ node.js
@@ -0,0 +1,13 @@
+let StartText = prompt('Ismingizni kiriting')
+let TwoTexts = prompt('Familiyangizni kiriting')
+let ThreeText = prompt('Yoshingizni kiriting')
+let FourText = prompt('Yaxshi korgan faningizni kiriting')
+let EndText = prompt('Yashaydigan tumaningizni kiriting')
+
+
+
+console.log('Ism:'+StartText);
+console.log('Familiya:'+TwoTexts);
+console.log('Yosh:'+ThreeText);
+console.log('Yaxshi korgan fan:'+FourText);
+console.log('Yashash joyi:'+EndText);
